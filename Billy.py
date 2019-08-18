d991dc47d244bbc362cea60c9b8c6f5f79d93496
SebastianCS <sebastian.cornejo.silva@gmail.com>
2019-08-17 23:38:08 -0400

    Se ha ordenado el codigo dentro de varios archivos para facilitar el mantenimiento del bot y que sea mas facil el agregar comandos

--- Billy.py
+++ Billy.py
@@ -1,61 +1,31 @@
 # Work with Python 3.6
 import random
 import asyncio
-
+import os
 import discord
 from discord.ext import commands
 
 TOKEN = ''
 
-PREFIX = '♂'
+PREFIX = ['♂','.']
 client = commands.Bot(command_prefix=PREFIX)
 
-@client.event
-async def on_ready():
-    game = discord.Game("♂ with his Duck ♂")
-    await client.change_presence(status=discord.Status.idle, activity=game)
-    print("\n Logged in as " + client.user.name)
-
-async def list_servers():
-    await client.wait_until_ready()
-    while not client.is_closed:
-        print("Current servers:")
-        for server in client.servers:
-            print(server.name)
-        await asyncio.sleep(600)
-
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'comandos.{extension}')
 
 @client.command()
-async def hola(message):
-    await message.send("Hola {0.author.mention}".format(message))
+async def unload(ctx, extension):
+    client.unload_extension(f'comandos.{extension}')
 
 @client.command()
-async def PepeHands(message):
-    await message.send("https://youtu.be/O3L-m7syRyI")
+async def reload(ctx, extension):
+    client.load_extension(f'comandos.{extension}')
+    client.unload_extension(f'comandos.{extension}')
 
-@client.command()
-async def gachiclasico(message):
-    await message.send("https://www.youtube.com/watch?v=JPxfAYYo7NA")
-""""
-@client.command()
-async def help(message):
-    await message.send("hola, PepeHands, gachiclasico, gachirandom")
-"""
-
-@client.command()
-async def gachirandom(message):
-
-    lista = ["https://youtu.be/O3L-m7syRyI",
-             "https://www.youtube.com/watch?v=JPxfAYYo7NA",
-             "https://www.youtube.com/watch?v=NdqbI0_0GsM",
-             "https://www.youtube.com/watch?v=ZqrbdXmVr44",
-             "https://www.youtube.com/watch?v=OWNOQA6fWC8",
-             "https://www.youtube.com/watch?v=_o2cr1BP-sk",
-             "https://www.youtube.com/watch?v=tJpjQqpbbY8",
-             "https://www.youtube.com/watch?v=SVHj64ltMb8",
-             "https://www.youtube.com/watch?v=m1_HHVoPjho"]
-    await message.send(random.choice(lista))
+for filename in os.listdir('./comandos'):
+    if filename.endswith('.py'):
+        client.load_extension(f'comandos.{filename[:-3]}')
 
 
-client.loop.create_task(list_servers())
 client.run(TOKEN)
--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Este bot es un proyecto que actualmente empece para estudiar el como funcionan t
 Actualmente tiene lo basico solamente para funcionar y espero agregar las conecciones a diversas APIs
 
 Se le acaba de crear un entorno virtual para que sea mas facil la ejecucion en diversos equipos
+
+Como iniciar el bot?
+tan solo inicia el entorno virtual y luego ejecuta Billy.py
